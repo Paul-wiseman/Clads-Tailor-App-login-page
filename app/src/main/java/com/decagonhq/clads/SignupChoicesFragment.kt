ae9de80a380718352f76f1a2089212e79b483820
decagon <anietieudoaka@gmail.com>
2021-06-01 19:43:42 +0100

    Fixed gradle build issues

--- app/src/main/java/com/decagonhq/clads/SignupChoicesFragment.kt
+++ app/src/main/java/com/decagonhq/clads/SignupChoicesFragment.kt
@@ -15,19 +15,29 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
-import timber.log.Timber
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import timber.log.Timber.*
 
 class SignupChoicesFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
     private var _binding: FragmentSignupChoicesBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?): View {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentSignupChoicesBinding.inflate(inflater, container, false)
         return binding.root
@@ -36,41 +46,68 @@ class SignupChoicesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
+
         googleSignInClient = GoogleSignIn.getClient(requireContext(), googleSignInOptions)
 
-        binding.signupChoicesFragmentGoogleSignupButton.setOnClickListener{
+        binding.signupChoicesFragmentGoogleSignupButton.setOnClickListener {
             signIn()
-        }
-    }
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, REQUEST_SIGN_IN)
-    }
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_SIGN_IN) {
-            try {
-                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-                handleSignInResult(task)
-            }
-            catch (e: ApiException) {
-                Toast.makeText(requireContext(), "Login Failed: ${e.statusCode}", Toast.LENGTH_SHORT).show()
-            }
         }
     }
 
     override fun onStart() {
         super.onStart()
-        val account = GoogleSignIn.getLastSignedInAccount(requireContext())
-        if (account != null){
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
             findNavController().navigate(R.id.action_signupChoicesFragment_to_signupEmailFragment)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == REQUEST_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+//                handleSignInResult(task)
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                d("firebaseAuthWithGoogle: ${account.id}")
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Toast.makeText(requireContext(), "Login Failed: ${e.statusCode}",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, REQUEST_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    d("signInWithCredential:success")
+                    val user = auth.currentUser
+                    val userEmail = user?.email
+                    val action =
+                        SignupChoicesFragmentDirections.actionSignupChoicesFragmentToSignupEmailFragment(userEmail)
+                    findNavController().navigate(action)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    tag("SignInFailure: ").w(task.exception, "signInWithCredential:failure")
+                }
+            }
     }
 
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
@@ -81,11 +118,10 @@ class SignupChoicesFragment : Fragment() {
             val action =
                 SignupChoicesFragmentDirections.actionSignupChoicesFragmentToSignupEmailFragment(accountEmail)
             findNavController().navigate(action)
-
-        } catch (e: ApiException) {
+        }
+        catch (e: ApiException) {
             // The ApiException status code indicates the detailed failure reason.
-            d("Sign in Error: ${e.statusCode}")
-
+            d("Google sign-in Error: ${e.statusCode}")
         }
     }
 
--- app/src/main/java/com/decagonhq/clads/SignupLoginFragment.kt
+++ app/src/main/java/com/decagonhq/clads/SignupLoginFragment.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.decagonhq.clads.databinding.FragmentSignupChoicesBinding
 import com.decagonhq.clads.databinding.FragmentSignupLoginBinding
 
 class SignupLoginFragment : Fragment() {
