3fe1d817f8c24ddd4eb00d1210f7f69a0c1da00a
Jesse <jesseonoyeyan@yahoo.com>
2021-06-02 14:12:55 +0100

    Added Features Splash Screen Landing Page Signup Options

---
+++ app/src/androidTest/java/com/decagonhq/clads/SignupLoginFragmentTest.kt
@@ -0,0 +1,66 @@
+package com.decagonhq.clads
+
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.* // ktlint-disable no-wildcard-imports
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.TestCase
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SignupLoginFragmentTest : TestCase() {
+
+    private lateinit var scenario: FragmentScenario<SignupLoginFragment>
+
+    @Before
+    fun setup() {
+        // Create a TestNavHostController
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+
+        // Create a graphical FragmentScenario for the Signup_Login Fragment
+        scenario = launchFragmentInContainer(themeResId = R.style.Theme_MaterialComponents)
+        scenario.moveToState(Lifecycle.State.STARTED)
+
+        scenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+
+            // Make the NavController available via the findNavController() APIs
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+    }
+
+    @Test
+    fun is_Signup_Fragment_Imageview_Visible() {
+        onView(withId(R.id.fragment_signup_login_image_view)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun is_Signup_Fragment_Textview_Visible() {
+        onView(withId(R.id.fragment_signup_login_textview)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun is_Signup_Fragment_Signup_Button_Visible() {
+        onView(withId(R.id.fragment_signup_signup_button)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun is_Signup_Fragment_Login_Button_Visible() {
+        onView(withId(R.id.fragment_signup_login_button)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testNavigationToSignupChoicesScreen() {
+
+        onView(withId(R.id.fragment_signup_signup_button)).perform(ViewActions.click())
+    }
+}
--- app/src/main/java/com/decagonhq/clads/SplashScreenActivity.kt
+++ app/src/main/java/com/decagonhq/clads/SplashScreenActivity.kt
@@ -3,21 +3,29 @@ package com.decagonhq.clads
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.decagonhq.clads.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+    // Declare binding variable
+    private var _binding: ActivitySplashScreenBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        _binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // Handler and Runnable execute in UIThread (don`t block main thread)
-        Handler().postDelayed(
+        Handler(Looper.getMainLooper()).postDelayed(
             {
-                val i = Intent(this@SplashScreenActivity, CladsActivity::class.java)
-                startActivity(i)
+                val intent = Intent(this, CladsActivity::class.java)
+                startActivity(intent)
                 finish()
             },
-            1000
+            1500
         )
     }
 }
