35cd7b20ea9246c7c2d9d07a07f83e77413e1dfd
segunoz <segunoz1@gmail.com>
2021-06-03 15:43:27 +0100

    Formmatted code with ktlint

--- app/src/androidTest/java/com/decagonhq/clads/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/decagonhq/clads/ExampleInstrumentedTest.kt
@@ -1,12 +1,8 @@
 package com.decagonhq.clads
 
-import androidx.test.core.app.ActivityScenario.launch
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import junit.framework.Assert.assertEquals
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -16,10 +12,8 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 
-
-//@get:Rule
-//var activityScenarioRule = activityScenarioRule<CladsActivity>()
-
+// @get:Rule
+// var activityScenarioRule = activityScenarioRule<CladsActivity>()
 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
@@ -30,13 +24,3 @@ class ExampleInstrumentedTest {
         assertEquals("com.decagonhq.clads", appContext.packageName)
     }
 }
-
-@RunWith(AndroidJUnit4::class)
-class MyTestSuite {
-    @Test fun testEventFragment() {
-        // The "fragmentArgs" argument is optional.
-
-        val scenario = launchFragment<EventFragment>
-    }
-}
-
--- app/src/main/java/com/decagonhq/clads/EmailVerificationFragment.kt
+++ app/src/main/java/com/decagonhq/clads/EmailVerificationFragment.kt
@@ -5,8 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.decagonhq.clads.databinding.FragmentEmailVerificationBinding
 
 class EmailVerificationFragment : Fragment() {
+
+    // using binding
+    private var preBinding: FragmentEmailVerificationBinding? = null
+    private val binding: FragmentEmailVerificationBinding get() = preBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +23,17 @@ class EmailVerificationFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        preBinding = FragmentEmailVerificationBinding.inflate(inflater, container, false)
+        return preBinding!!.root
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_email_verification, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.emailVerificationFragmentVerifyEmailAddressButton.setOnClickListener {
+            findNavController().navigate(R.id.action_emailVerificationFragment_to_loginFragment)
+        }
     }
 }
